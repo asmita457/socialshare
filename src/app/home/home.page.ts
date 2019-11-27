ab3d7c7fb6cdba8105480374b38bf69110f0405f
Asmita Belhekar <asmita.belhekar@arraypointer.com>
2019-11-27 14:34:25 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { NavController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,167 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
 
+  public sendTo   : any;
+  public subject  : string = 'Message from Social Sharing App';
+  public message  : string = 'Take your app development skills to the next level with Mastering Ionic - the definitive guide';
+  public image    : string	= 'Message from Social Sharing App';
+  public uri      : string	= 'https://play.google.com/store/apps/details?id=com.tracey.barrierbuster&hl=en';
+
+  constructor(private socialSharing: SocialSharing,
+    public navCtrl    : NavController,
+    public platform   : Platform) { }
+
+
+  share() {
+    // alert("share");
+    // this.socialSharing.canShareViaEmail().then(() => {
+    //   // Sharing via email is possible
+    // }).catch(() => {
+    //   // Sharing via email is not possible
+    // });
+    
+    // // Share via email
+    // this.socialSharing.shareViaEmail('Body', 'Subject', ['recipient@example.org']).then(() => {
+    //   // Success!
+    // }).catch(() => {
+    //   // Error!
+    // });
+
+    // this.socialSharing.shareViaWhatsAppToReceiver
+    this.socialSharing.shareVia('instagram','abc', 'Subject', 'recipient@example.org','xyz').then(() => {
+
+    }).catch(() => {
+      // Error!
+    });
+  }
+
+  shareViaEmail()
+   {
+      this.platform.ready()
+      .then(() =>
+      {
+        this.socialSharing.canShareViaEmail()
+         .then(() =>
+         {
+          this.socialSharing.shareViaEmail(this.message, this.subject, this.sendTo)
+            .then((data) =>
+            {
+               console.log('Shared via Email');
+            })
+            .catch((err) =>
+            {
+               console.log('Not able to be shared via Email');
+            });
+         })
+         .catch((err) =>
+         {
+            console.log('Sharing via Email NOT enabled');
+         });
+      });
+   }
+
+   shareViaFacebook()
+   {
+      this.platform.ready()
+      .then(() =>
+      {
+        this.socialSharing.canShareVia('com.apple.social.facebook', this.message, this.image, this.uri)
+         .then((data) =>
+         {
+
+          this.socialSharing.shareViaFacebook(this.message, this.image, this.uri)
+            .then((data) =>
+            {
+               console.log('Shared via Facebook');
+            })
+            .catch((err) =>
+            {
+               console.log('Was not shared via Facebook');
+            });
+
+         })
+         .catch((err) =>
+         {
+            console.log('Not able to be shared via Facebook');
+         });
+
+      });
+   }
+
+
+
+
+   shareViaInstagram()
+   {
+      this.platform.ready()
+      .then(() =>
+      {
+
+        this.socialSharing.shareViaInstagram(this.message, this.image)
+         .then((data) =>
+         {
+            console.log('Shared via shareViaInstagram');
+         })
+         .catch((err) =>
+         {
+            console.log('Was not shared via Instagram');
+         });
+
+      });
+   }
+
+
+
+
+   sharePicker()
+   {
+      this.platform.ready()
+      .then(() =>
+      {
+
+        this.socialSharing.share(this.message, this.subject, this.image, this.uri)
+         .then((data) =>
+         {
+            console.log('Shared via SharePicker');
+         })
+         .catch((err) =>
+         {
+            console.log('Was not shared via SharePicker');
+         });
+
+      });
+   }
+
+
+
+
+   shareViaTwitter()
+   {
+      this.platform.ready()
+      .then(() =>
+      {
+
+        this.socialSharing.canShareVia('com.apple.social.twitter', this.message, this.image, this.uri)
+         .then((data) =>
+         {
+
+          this.socialSharing.shareViaTwitter(this.message, this.image, this.uri)
+            .then((data) =>
+            {
+               console.log('Shared via Twitter');
+            })
+            .catch((err) =>
+            {
+               console.log('Was not shared via Twitter');
+            });
+
+         });
+
+      })
+      .catch((err) =>
+      {
+         console.log('Not able to be shared via Twitter');
+      });
+   }
 }
